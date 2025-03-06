c7565f58be505fe9ebc898d1e17a710ee04c5f03
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-03-06 17:57:13 -0300

    Login and Register Pages code

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,20 @@
 import styles from "./style.module.scss";
 
 export const Login: React.FC = () => {
-  return <h1>LOGin</h1>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <form>
+          <div className={styles.inputBox}>
+            <label>Email</label>
+            <input type="email" />
+          </div>
+          <div className={styles.inputBox}>
+            <label>Password</label>
+            <input type="password" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,5 +1,28 @@
 import styles from "./style.module.scss";
 
 export const Register: React.FC = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <form>
+          <div className={styles.inputBox}>
+            <label>Name</label>
+            <input type="text" />
+          </div>
+          <div className={styles.inputBox}>
+            <label>Email</label>
+            <input type="email" />
+          </div>
+          <div className={styles.inputBox}>
+            <label>Password</label>
+            <input type="password" />
+          </div>
+          <div className={styles.inputBox}>
+            <label>Confirm password</label>
+            <input type="password" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
