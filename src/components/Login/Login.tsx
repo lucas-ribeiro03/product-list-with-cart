06f8d20254ec67d7f4e89cecfa2a6048ddbe0925
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-03-06 17:48:55 -0300

    Login and Register page creation

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,5 @@
+import styles from "./style.module.scss";
+
+export const Login: React.FC = () => {
+  return <h1>LOGin</h1>;
+};
---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,5 @@
+import styles from "./style.module.scss";
+
+export const Register: React.FC = () => {
+  return <h1>Hello world</h1>;
+};
