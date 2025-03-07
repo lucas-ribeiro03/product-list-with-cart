34370adbef79272670d09113c781bb52cf8b53f4
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-03-07 18:52:30 -0300

    route fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,47 @@ import styles from "./styles/global.module.scss";
 import { ProductList } from "./components/ProductList/ProductList";
 import { Modal } from "./components/ConfirmationModal/Modal";
 import { Cart } from "./components/Cart/Cart";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <>
-      <Provider store={store}>
+    <Provider store={store}>
+      <Router>
         <div className={styles.global}>
-          <ProductList />
-          <Cart openModal={() => setIsModalOpen(true)} />
-        </div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <div style={{ gridArea: "box1" }} className={styles.navbar}>
+                    <Navbar />
+                  </div>
+                  <div
+                    style={{ gridArea: "box2" }}
+                    className={styles.productList}
+                  >
+                    <ProductList />
+                  </div>
+                  <div style={{ gridArea: "box3" }} className={styles.cart}>
+                    <Cart openModal={() => setIsModalOpen(true)} />
+                  </div>
 
-        {isModalOpen && <Modal closeModal={() => setIsModalOpen(false)} />}
-      </Provider>
-    </>
+                  {isModalOpen && (
+                    <Modal closeModal={() => setIsModalOpen(false)} />
+                  )}
+                </>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
