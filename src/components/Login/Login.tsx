4fa6dcb43454dabd303fbfbb80e64a0d30e54e88
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-03-07 18:49:57 -0300

    Login page done

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,18 +1,61 @@
 import styles from "./style.module.scss";
+import { RootReducer } from "../../redux/root-reducer";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export const Login: React.FC = () => {
+  const user = useSelector(
+    (rootReducer: RootReducer) => rootReducer.UserReducer
+  );
+
+  const [loginUser, setLoginUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!user.user) {
+      alert("usuario nao existe");
+    } else if (user.user?.password !== loginUser.password) {
+      alert("incorrect password");
+    } else if (user.user?.email !== loginUser.email) {
+      alert("incorrect email");
+    } else {
+      console.log("concluido");
+      navigate("/");
+    }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className={styles.inputBox}>
             <label>Email</label>
-            <input type="email" />
+            <input
+              type="email"
+              onChange={(e) =>
+                setLoginUser({ ...loginUser, email: e.target.value })
+              }
+              value={loginUser.email}
+            />
           </div>
           <div className={styles.inputBox}>
             <label>Password</label>
-            <input type="password" />
+            <input
+              type="password"
+              onChange={(e) =>
+                setLoginUser({ ...loginUser, password: e.target.value })
+              }
+              value={loginUser.password}
+            />
           </div>
+
+          <button className={styles.btnLogin}>Login</button>
         </form>
       </div>
     </div>
